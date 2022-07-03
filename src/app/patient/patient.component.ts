3969c5eda253a14a2d8602329511543ed54af608
Johan Nadaud <johan.nadaud.pro@gmail.com>
2022-07-03 18:15:01 +0200

    Add routing module and header.

---
+++ src/app/patient/patient.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {PatientModel} from "../shared/patient.model";
+import {PatientService} from "../shared/patient.service";
+import {Observable} from "rxjs";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-patient',
+  templateUrl: './patient.component.html',
+  styleUrls: ['./patient.component.scss']
+})
+export class PatientComponent implements OnInit{
+  patient$!: Observable<PatientModel>;
+  patientForm! : FormGroup;
+  //show=false;
+
+  constructor(private patientService: PatientService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.patientForm = this.formBuilder.group({
+      lastName: ['', {validators: Validators.required}],
+      firstName: ['', {validators: Validators.required}],
+    })
+  }
+
+  onSubmit() {
+  this.patient$ = this.patientService.getPatient(this.patientForm.value.firstName, this.patientForm.value.lastName);
+  //this.show=true;
+  }
+
+}
